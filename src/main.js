6ff9a2c506663646f8864d014f0c1d127ccbd548
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-01-22 10:02:45 +0100

    feat(add delete function)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import './styles/style.scss'
 
-// let userList = document.querySelector('#userList');
+/**
+ * Selectors
+ */
 let userForm = document.querySelector('#userForm');
 let registerForm = document.querySelector('#register')
 
+/**
+ * Check if there is a user in localStorage
+ */
 if(localStorage.getItem("user")){
+    const userInLocalStorage = JSON.parse(localStorage.getItem("user"));
     printlogOutButton()
+    printLoggedInUser(userInLocalStorage);
+    printDeleteUserButton(userInLocalStorage);
     
 } else {
     printLogInForm()
-    // add delite user function
 }
+
+/**
+ * Functions 
+ */
 
 function printRegisterform(){
     userForm.innerHTML = '';
@@ -34,7 +45,7 @@ function printRegisterform(){
 }
 
 function checkIfUserExist(name, email, password){
-    let inputRegisterUser = {name: name.value, email: email.value, password: password.value}
+    let inputRegisterUser = { email: email.value}
 
     fetch('http://localhost:3000/users/check',{
         method: "POST",
@@ -76,6 +87,7 @@ function registerUser(name, email, password){
 
 function printLogInForm(){
     userForm.innerHTML = '';
+    registerForm.innerHTML = '';
     let logInHeading = document.createElement('h1');
     logInHeading.innerHTML = 'Logga in'
     let inputEmail = document.createElement('input');
@@ -96,7 +108,9 @@ function printLogInForm(){
 
     userForm.append(logInHeading, inputEmail,inputPassword,loginButton, registerButton)
 
+    
 }
+
 
 function getLogInFromAPI(inputEmail, inputPassword){
     let sendUser = {email: inputEmail.value, password: inputPassword.value}
@@ -114,14 +128,54 @@ function getLogInFromAPI(inputEmail, inputPassword){
         if(data.user){
             localStorage.setItem("user" , JSON.stringify(data));
             printlogOutButton();
-            printLoggedInUser(data)
+            printLoggedInUser(data);
+            printDeleteUserButton(data);
         }else {
             alert('fel inlogg')
         }
       
     })
+  
     
 };
+
+function printDeleteUserButton(data){
+    let deleteUserButton = document.createElement('button');
+    deleteUserButton.innerText = 'Ta bort användare';
+
+    deleteUserButton.addEventListener('click', ()=>{
+        deleteUser(data);
+        localStorage.removeItem("user");
+    })
+
+    userForm.appendChild(deleteUserButton);
+    
+}
+
+
+function deleteUser(data){
+    userForm.innerHTML = '';
+    const deletedUser = data.user;
+    const deletedUserMail = data.email;
+    let id = data.id;
+
+    fetch('http://localhost:3000/users' +'/' + id, {
+        method : "DELETE"
+    })
+
+    printLogInForm();
+    let deletedMessage = document.createElement('p');
+    deletedMessage.innerText = `Användaren ${deletedUser} med mailen ${deletedUserMail} är nu borttagen, vänligen logga in med en annan användare eller resistrera en ny`;
+
+    userForm.appendChild(deletedMessage);
+    setTimeout(() => {
+        deletedMessage.innerText ='';
+    }, 5000);
+    
+
+
+   
+}
 
 function printLoggedInUser(data){
     let loggedInInfo = document.createElement('p');
